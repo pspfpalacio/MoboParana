ab4f7b2f5060ec572a3206b57c414f1485813880
epagolaktz <epagola@kaitzen-solutions.com>
2017-09-05 09:43:57 -0300

    nueva entidad

---
+++ src/model/entity/EquipoPendientePago.java
@@ -0,0 +1,161 @@
+package model.entity;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.Date;
+
+
+/**
+ * The persistent class for the equipo_pendiente_pago database table.
+ * 
+ */
+@Entity
+@Table(name="equipo_pendiente_pago")
+@NamedQuery(name="EquipoPendientePago.findAll", query="SELECT e FROM EquipoPendientePago e")
+public class EquipoPendientePago implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+
+	private boolean enabled;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name="fecha_alta")
+	private Date fechaAlta;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name="fecha_baja")
+	private Date fechaBaja;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name="fecha_mod")
+	private Date fechaMod;
+
+	private String imei;
+
+	private float monto;
+
+	private boolean pagado;
+
+	//bi-directional many-to-one association to Cliente
+	@ManyToOne
+	@JoinColumn(name="id_cliente")
+	private Cliente cliente;
+
+	//bi-directional many-to-one association to Usuario
+	@ManyToOne
+	@JoinColumn(name="id_usuario_alta")
+	private Usuario usuario1;
+
+	//bi-directional many-to-one association to Usuario
+	@ManyToOne
+	@JoinColumn(name="id_usuario_mod")
+	private Usuario usuario2;
+
+	//bi-directional many-to-one association to Usuario
+	@ManyToOne
+	@JoinColumn(name="id_usuario_baja")
+	private Usuario usuario3;
+
+	public EquipoPendientePago() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public boolean getEnabled() {
+		return this.enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public Date getFechaAlta() {
+		return this.fechaAlta;
+	}
+
+	public void setFechaAlta(Date fechaAlta) {
+		this.fechaAlta = fechaAlta;
+	}
+
+	public Date getFechaBaja() {
+		return this.fechaBaja;
+	}
+
+	public void setFechaBaja(Date fechaBaja) {
+		this.fechaBaja = fechaBaja;
+	}
+
+	public Date getFechaMod() {
+		return this.fechaMod;
+	}
+
+	public void setFechaMod(Date fechaMod) {
+		this.fechaMod = fechaMod;
+	}
+
+	public String getImei() {
+		return this.imei;
+	}
+
+	public void setImei(String imei) {
+		this.imei = imei;
+	}
+
+	public float getMonto() {
+		return this.monto;
+	}
+
+	public void setMonto(float monto) {
+		this.monto = monto;
+	}
+
+	public boolean getPagado() {
+		return this.pagado;
+	}
+
+	public void setPagado(boolean pagado) {
+		this.pagado = pagado;
+	}
+
+	public Cliente getCliente() {
+		return this.cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Usuario getUsuario1() {
+		return this.usuario1;
+	}
+
+	public void setUsuario1(Usuario usuario1) {
+		this.usuario1 = usuario1;
+	}
+
+	public Usuario getUsuario2() {
+		return this.usuario2;
+	}
+
+	public void setUsuario2(Usuario usuario2) {
+		this.usuario2 = usuario2;
+	}
+
+	public Usuario getUsuario3() {
+		return this.usuario3;
+	}
+
+	public void setUsuario3(Usuario usuario3) {
+		this.usuario3 = usuario3;
+	}
+
+}
