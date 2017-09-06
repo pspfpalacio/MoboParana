7d8a31942c6c3380f4efe926f610bbec67843158
chuncho <efrancisco.pagola@gmail.com>
2017-09-05 23:25:14 -0300

    metodos DAO EquipoPendientePago

--- src/dao/impl/DAOEquipoPendientePagoImpl.java
+++ src/dao/impl/DAOEquipoPendientePagoImpl.java
@@ -1,12 +1,16 @@
 package dao.impl;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
+import model.entity.Caja;
+import model.entity.Cliente;
 import model.entity.EquipoPendientePago;
 import dao.interfaces.DAOEquipoPendientePago;
 
@@ -43,6 +47,85 @@ public class DAOEquipoPendientePagoImpl  implements Serializable, DAOEquipoPendi
 			tx.rollback();
 			return 0;
 		}
+	}
+	
+	public int update(EquipoPendientePago epp) {
+		inicializar();
+		EntityTransaction tx = em.getTransaction();
+		try {
+			String update = "UPDATE EquipoPendientePago e SET e.pagado = :pPagado, e.usuarioMod = :pUsuarioMod, e.fechaMod = :pFechaMod "
+					+ "WHERE e.id = :pId";
+			Query locQuery = em.createQuery(update);
+			locQuery.setParameter("pPagado", epp.getPagado());
+			locQuery.setParameter("pFechaMod", epp.getFechaMod());
+			locQuery.setParameter("pUsuarioMod", epp.getUsuario2());
+			locQuery.setParameter("pId", epp.getId());
+			tx.begin();
+			locQuery.executeUpdate();
+			tx.commit();
+			return epp.getId();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			tx.rollback();
+			return 0;
+		}
+	}
+	
+	public int baja(EquipoPendientePago epp) {
+		inicializar();
+		EntityTransaction tx = em.getTransaction();
+		try {
+			String update = "UPDATE EquipoPendientePago e SET e.enabled = 0 "
+					+ "WHERE e.id = :pId";
+			Query locQuery = em.createQuery(update);
+			locQuery.setParameter("pId", epp.getId());
+			tx.begin();
+			locQuery.executeUpdate();
+			tx.commit();
+			return epp.getId();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			tx.rollback();
+			return 0;
+		}
+	}
+	
+	public EquipoPendientePago getPorImei(String imei) {
+		inicializar();
+		Query locQuery = em.createQuery("SELECT e FROM EquipoPendientePago e WHERE e.imei = :pImei", EquipoPendientePago.class);
+		locQuery.setParameter("pImei", imei);
+		EquipoPendientePago epp = new EquipoPendientePago();
+		try{
+			epp = (EquipoPendientePago) locQuery.getSingleResult();
+		} catch (Exception e) {
+			System.out.println("getCliente " + e.getMessage());
+			epp = new EquipoPendientePago();
+		}
+		return epp;
+	}
+	
+	public List<EquipoPendientePago> getListaPorCliente(Cliente cliente) {
+		inicializar();
+		Query locQuery = em.createQuery("SELECT e FROM EquipoPendientePago e WHERE e.cliente = :pCliente", EquipoPendientePago.class);
+		locQuery.setParameter("pCliente", cliente.getId());
+		List<EquipoPendientePago> lista = locQuery.getResultList();
+		return lista;
+	}
+	
+	public List<EquipoPendientePago> getListaNoPagosPorCliente(Cliente cliente) {
+		inicializar();
+		Query locQuery = em.createQuery("SELECT e FROM EquipoPendientePago e WHERE e.cliente = :pCliente and e.pago = 0", EquipoPendientePago.class);
+		locQuery.setParameter("pCliente", cliente.getId());
+		List<EquipoPendientePago> lista = locQuery.getResultList();
+		return lista;
+	}
+	
+	public List<EquipoPendientePago> getListaPagosPorCliente(Cliente cliente) {
+		inicializar();
+		Query locQuery = em.createQuery("SELECT e FROM EquipoPendientePago e WHERE e.cliente = :pCliente and e.pago = 1", EquipoPendientePago.class);
+		locQuery.setParameter("pCliente", cliente.getId());
+		List<EquipoPendientePago> lista = locQuery.getResultList();
+		return lista;
 	}
 
 }
--- src/dao/interfaces/DAOEquipoPendientePago.java
+++ src/dao/interfaces/DAOEquipoPendientePago.java
@@ -1,9 +1,23 @@
 package dao.interfaces;
 
-import model.entity.EquipoPendientePago;;
+import model.entity.EquipoPendientePago;
+import model.entity.Cliente;
+import java.util.List;
 
 public interface DAOEquipoPendientePago {
 	
 	public int insert(EquipoPendientePago equipoPendientePago);
+	
+	public int update(EquipoPendientePago equipoPendientePago);
+	
+	public int baja(EquipoPendientePago equipoPendientePago);
+	
+	public EquipoPendientePago getPorImei(String imei);
+	
+	public List<EquipoPendientePago> getListaPorCliente(Cliente cliente);
+	
+	public List<EquipoPendientePago> getListaNoPagosPorCliente(Cliente cliente);
+	
+	public List<EquipoPendientePago> getListaPagosPorCliente(Cliente cliente);
 
 }
